d4ab4fa6253cf226026afcf4a466fcffd42bc963
stasbertolit228 <staspetych228@gmail.com>
2024-10-17 15:37:00 +0300

    first

---
+++ src/Main.kt
@@ -0,0 +1,37 @@
+fun main() {
+    val student:student? = student()//null
+    student?.let { st ->
+        st.hi()
+        st.bye()
+        st.name = "asdasa"
+
+    }
+    println(student?.name)
+
+    //apply
+    val student2 = student?.apply { // работает с параметром объекта
+        hi()
+        bye()
+        name = "DDDDDD"
+
+    }
+    println(student?.name)
+    println(student2?.name)
+
+    //run
+    val Agestudent = student().run {
+        hi()
+        bye()
+        name = "BFEBFEBFEFBEE"
+        age = 10
+        age
+    }
+    println(Agestudent)
+
+    val student4 = student?.also { st->
+        st.bye()
+        st.hi()
+        st.name = "masa"
+    }
+    println(student4?.name)
+}
---
+++ src/student.kt
@@ -0,0 +1,10 @@
+class student {
+    var name:String = "Sasasas"
+    var age:Int = 0
+    fun hi(){
+        println("hi")
+    }
+    fun bye(){
+        println("Bye")
+    }
+}
